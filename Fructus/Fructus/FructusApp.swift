00901aaf3aa0312a7b6f35d74e9445acef95aff2
Aryaveer Bajpai <aryaveerbajpai@gmail.com>
2022-12-16 15:51:34 +0530

    Created Project for Fractus App

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Unthinkable-mac-0077 on 16/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
